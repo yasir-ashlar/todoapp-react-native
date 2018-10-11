aa3d4ae5499d0e5038fcbff15cb7a5b8ff3e26a9
Yasir Lateef <yasir.ashlar@gmail.com>
2018-10-11 17:59:35 +0500

    Add RNGooglePlace for GMaps Autocomplete

--- android/app/src/main/java/com/ashlarglobal/todoapp/MainApplication.java
+++ android/app/src/main/java/com/ashlarglobal/todoapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -45,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
+            new RNGooglePlacesPackage(),
         new RNFirebasePackage(),
         new MapsPackage(),
         // add/remove these packages as appropriate
--- app/Screens/TodoDetailsScreen.js
+++ app/Screens/TodoDetailsScreen.js
@@ -1,10 +1,10 @@
 import React,{ Component }  from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text, PermissionsAndroid } from 'react-native'
 import { Container, Button, Content, Form, Item, Input, Label } from 'native-base';
 // Your own deps
 import firebase from 'react-native-firebase'
-import MapView from 'react-native-maps';
-
+import MapView, { Marker } from 'react-native-maps';
+import RNGooglePlaces from 'react-native-google-places'
 export default class TodoDetailScreen extends Component{
     constructor(props) {
         //Initial State stuff?
@@ -14,16 +14,44 @@ export default class TodoDetailScreen extends Component{
         const {text, key} = todoItem
         this.state = {
             todoText: text,
-            todoID: key
+            todoID: key,
+            latitude: 0,
+            longitude: 0,
         }
     }
 
     componentDidMount() {
+        canUseLocation = this.requestLocationPermissions();
         
+        if(canUseLocation){
+            RNGooglePlaces.getCurrentPlace()
+            .then((results) => { this.setState({ latitude: results[0].latitude, longitude: results[0].longitude}) })
+            .catch((error) => alert(error.message));
+        }
     }
     static navigationOptions = {
         title: 'Edit Todo Item'
     }
+
+    requestLocationPermissions = async () => {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              'title': 'TodoApp Location Services Permission',
+              'message': 'Cool Photo App needs access to your current location ' +
+                         'so you can have the best experience.'
+            }
+          )
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            return true
+          } else {
+            return;
+          }
+        } catch (err) {
+          return;
+        }
+      }
 
     updateTodoItem = () => {
         const { navigation } = this.props;
@@ -41,6 +69,21 @@ export default class TodoDetailScreen extends Component{
         this.setState({todoText: event.target.value})
     }
 
+    openSearchModal() {
+        RNGooglePlaces.openPlacePickerModal()
+        .then((place) => {
+            // alert(JSON.parse(place));
+            // place represents user's selection from the
+            // suggestions and it is a simplified Google Place object.
+            const {latitude, longitude} = place; 
+            this.setState({
+                latitude,
+                longitude
+            })
+        })
+        .catch(error => console.log(error.message));  // error is a Javascript Error object
+    }
+
     render(){
        
         return(
@@ -52,18 +95,35 @@ export default class TodoDetailScreen extends Component{
                             <Input value={this.state.todoText} onChange={ () => this.setState({todoText: event.target.value})  } />
                         </Item>
 
-                         <Button style={styles.updateButton} onPress={this.updateTodoItem}>
-                                <Text>Update</Text>
-                        </Button>
+                        
+                            <Button onPress={() => this.openSearchModal() }>
+                                <Text>Update Location</Text>
+                            </Button>
+
+                         
                     </Form>
                     <MapView style={styles.map}
-                initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}
-            />
+                        showUserLocation
+                        loadingEnabled
+                        region={{
+                            latitude: this.state.latitude,
+                            longitude: this.state.longitude,
+                            latitudeDelta: 0.0019,
+                            longitudeDelta: 0.0019,
+                        }}
+                    >
+                        <Marker 
+                            
+                            coordinate={{
+                                latitude: this.state.latitude,
+                                longitude: this.state.longitude                                                                                                                                                                                     
+                            }}
+                        />   
+                    </MapView>
+
+                    <Button style={styles.updateButton} onPress={this.updateTodoItem}>
+                            <Text>Update</Text>
+                    </Button>
                 </Content>
             </Container>
         )
@@ -80,7 +140,7 @@ const styles = StyleSheet.create({
         
     },
     map: {
-        height: 150,
+        height: 250,
         width: '100%'
     }
 })
